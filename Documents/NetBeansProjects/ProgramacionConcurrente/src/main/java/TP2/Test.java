7e92ae5d8d9bdde0ed651f34ca329713384da7ab
mausawicki7 <mau.sawicki@gmail.com>
2020-09-02 11:56:34 -0300

    Creo la clase Test de la Clase PingPong

---
+++ Documents/NetBeansProjects/ProgramacionConcurrente/src/main/java/TP2/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2;
+
+/**
+ *
+ * @author mausa
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        PingPong t1 = new PingPong("PING", 33);
+        PingPong t2 = new PingPong("PONG", 10);
+
+        t1.start();
+        t2.start();
+
+    }
+}
