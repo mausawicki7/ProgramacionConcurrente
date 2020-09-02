de47d6383b8033a6f6547bcc81f02cb2d276155a
mausawicki7 <mau.sawicki@gmail.com>
2020-09-02 10:34:02 -0300

    Clase cliente tp2 concurrente

---
+++ Documents/NetBeansProjects/ProgramacionConcurrente/src/main/java/TP2/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2;
+
+/**
+ *
+ * @author mausa
+ */
+public class Cliente extends Thread {
+    public void run(){
+        System.out.println("soy"+Thread.currentThread().getName());
+        Recurso.uso();
+        
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            
+        }
+    }
+    
+}
