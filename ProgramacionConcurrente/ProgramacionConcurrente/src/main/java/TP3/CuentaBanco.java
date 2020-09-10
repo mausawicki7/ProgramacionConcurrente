20431ceda4c7c3da54de557fa8c514278ed99a99
mausa <mausa@192.168.0.49>
2020-09-10 11:45:40 -0300

    TP3 desde PC Escritorio

---
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP3/CuentaBanco.java
@@ -0,0 +1,17 @@
+package TP3;
+
+public class CuentaBanco {
+
+    private int balance = 50;
+
+    public CuentaBanco() {
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void retiroBancario(int retiro) {
+        balance = balance - retiro;
+    }
+}
