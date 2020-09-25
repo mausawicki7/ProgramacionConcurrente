cf2e2234ffb33ec39b1b8188f2b4cb0d9932ba75
mausawicki7 <mau.sawicki@gmail.com>
2020-09-25 16:59:13 -0300

    Se corrigio la cantidad de letras ahora es variable

---
+++ Documents/NetBeansProjects/ProgConcurrente/src/main/java/TP4/Punto6/ClaseC.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.Punto6;
+
+/**
+ *
+ * @author mausa
+ */
+public class ClaseC implements Runnable {
+
+    Escritora x;
+    private int cant;
+
+    public ClaseC(Escritora x, int cantVeces) {
+        this.x = x;
+        this.cant = cantVeces;
+    }
+
+    @Override
+    public void run() {
+        int cantVeces = x.getValor();
+        for (int i = 0; i < cantVeces; i++) {
+            x.imprimirA(cant);
+        }
+    }
+
+}
