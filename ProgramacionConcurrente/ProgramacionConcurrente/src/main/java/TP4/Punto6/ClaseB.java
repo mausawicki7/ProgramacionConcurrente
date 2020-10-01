8a832a95d152b56c3f8b9bf34a75f5cf47eb8915
mausa <mausa@192.168.0.49>
2020-10-01 11:18:15 -0300

    Cantidad de veces a imprimir es variable

---
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP4/Punto6/ClaseB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.Punto6;
+
+/**
+ *
+ * @author mausa
+ */
+public class ClaseB implements Runnable {
+    
+    private int cant;
+    private Escritora i;
+
+    public ClaseB(int cant, Escritora i) {
+        this.cant = cant;
+        this.i = i;
+    }
+    
+    
+    @Override
+    public void run() {
+        int cantI = i.getValor();
+        for(int x=0; x<cantI; x++){
+            i.imprimirB(cant);
+        }
+       
+    }
+    
+}
---
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP4/Punto6/ClaseC.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.Punto6;
+
+/**
+ *
+ * @author mausa
+ */
+public class ClaseC implements Runnable {
+        
+    private int cant;
+    private Escritora i;
+
+    public ClaseC(int cant, Escritora i) {
+        this.cant = cant;
+        this.i = i;
+    }
+    
+    
+    @Override
+    public void run() {
+        int cantI = i.getValor();
+        for(int x=0; x<cantI; x++){
+            i.imprimirC(cant);
+        }
+       
+    }
+}
