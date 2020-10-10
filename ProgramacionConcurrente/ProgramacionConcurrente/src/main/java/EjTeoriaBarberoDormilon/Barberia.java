b71615938c04e998811c84ba748ad102bf42c830
mausa <mausa@192.168.0.49>
2020-10-10 14:44:28 -0300

    Barberia con sala de espera

--- ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/EjTeoriaBarberoDormilon/Barberia.java
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/EjTeoriaBarberoDormilon/Barberia.java
@@ -14,19 +14,23 @@ import java.util.logging.Logger;
  * @author mausa
  */
 public class Barberia {
-    
-    //Lo mas recomendable es que la sincronizacion la haga el recurso compartido
-     //Como en este caso al Barberia es el recurso compartido y aqui tengo los semaforos
 
+    //Lo mas recomendable es que la sincronizacion la haga el recurso compartido
+    //Como en este caso al Barberia es el recurso compartido entre el Barbero y el Cliente y aqui tengo los semaforos
     Semaphore semSillon;
     Semaphore semBarbero;
     Semaphore semCliente;
+    Semaphore mutexSillas;
     String miNombre;
+    int sillasLibres;
 
-    public Barberia(String nombre) {
-        miNombre = nombre;
+    public Barberia(String nombre, int totalSillas) {
+        this.miNombre = nombre;
+        this.sillasLibres = totalSillas;
         //Inicializo los semaforos en true para que el barbero le corte el pelo a los clientes que estan esperando para ser atendidos
         //en orden de llegada.
+
+        mutexSillas = new Semaphore(1, true);        //Semaforo para lograr la exclusion mutua
         semSillon = new Semaphore(1, true);
         semBarbero = new Semaphore(0, true);
         semCliente = new Semaphore(0, true);
@@ -34,22 +38,48 @@ public class Barberia {
     }
 
     public boolean entrar(String nombreCliente) {
+        boolean pudeEntrar = false;
         System.out.println("------------- soy " + nombreCliente + " estoy entrando.");
-        return (semSillon.tryAcquire());
-    }
-
-    public void solicitarCorte(String nombreCliente) {
-        System.out.println("Soy " + nombreCliente + " necesito un corte de pelo.");
-        semBarbero.release();
-
         try {
-            semCliente.acquire();
+            // SECCION CRITICA
+            mutexSillas.acquire();
+            //El cliente verifica si hay sillas libres
+            if (sillasLibres > 0) {
+                //Ocupa una silla
+                sillasLibres--;
+                pudeEntrar = true;
+            }else{
+                System.out.println("Soy "+nombreCliente+" no encontre sillas disponibles para sentarme. ME VOY!");
+            }
         } catch (InterruptedException ex) {
             Logger.getLogger(Barberia.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        mutexSillas.release();
+        return (pudeEntrar);
+    }
+
+    public void solicitarCorte(String nombreCliente) {
+        try {
+            // SECCION CRITICA
+            semSillon.acquire();
+            mutexSillas.acquire(); 
+            sillasLibres++;
+            mutexSillas.release();
+            
+            
+            System.out.println("Soy " + nombreCliente + " necesito un corte de pelo. Voy a despertar al barbero.");
+            semBarbero.release();
+            semCliente.acquire();
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Barberia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     public void esperarCliente(String nombreBarbero) {
+        System.out.println("Me voy a descansar un rato!");
         try {
             semBarbero.acquire();
         } catch (InterruptedException ex) {
