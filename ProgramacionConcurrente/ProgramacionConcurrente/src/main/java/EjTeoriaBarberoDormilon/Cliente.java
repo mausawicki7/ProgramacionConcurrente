f1e3caa58d5eb6f16261240f1cf7b65af8420420
mausa <mausa@192.168.0.49>
2020-10-10 14:46:00 -0300

    Cliente rendevouz

--- ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/EjTeoriaBarberoDormilon/Cliente.java
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/EjTeoriaBarberoDormilon/Cliente.java
@@ -10,34 +10,32 @@ package EjTeoriaBarberoDormilon;
  * @author mausa
  */
 public class Cliente extends Persona implements Runnable {
-    
+
     Barberia laBarberia;
-    
-    public Cliente(String nombre, Barberia bd){
+
+    public Cliente(String nombre, Barberia bd) {
         super(nombre);
         laBarberia = bd;
     }
 
-    private void caminarHastaBarberia(){
-        System.out.println("Soy "+nombre+" y estoy caminando hasta la barberia "+laBarberia.miNombre);
-        
-        try{
+    private void caminarHastaBarberia() {
+        System.out.println("Soy " + nombre + " y estoy caminando hasta la barberia " + laBarberia.miNombre);
+
+        try {
             Thread.sleep((int) (Math.random() * 300));
-        }catch(InterruptedException e){
-            
+        } catch (InterruptedException e) {
+
         }
-    }    
+    }
 
     @Override
     public void run() {
-       this.caminarHastaBarberia();
-        if(laBarberia.entrar(nombre)){
+        this.caminarHastaBarberia();
+        if (laBarberia.entrar(nombre)) {
             laBarberia.solicitarCorte(nombre);
-            System.out.println("Soy "+nombre+" ya me atendieron, me voy..");
+            System.out.println("Soy " + nombre + " ya me atendieron, me voy..");
             laBarberia.salir(nombre);
-        }else{
-            System.out.println("Soy "+nombre+" y el barbero me dice que esta ocupado..");
+
         }
     }
-    
 }
