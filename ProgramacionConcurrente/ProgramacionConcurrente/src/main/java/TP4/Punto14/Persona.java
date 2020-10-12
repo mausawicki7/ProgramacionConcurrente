a1cb005cfa4aacc34c9f4594dc9442da85631467
mausa <mausa@192.168.0.49>
2020-10-12 11:53:34 -0300

    Clase Persona heredan de ella Mozo, Empleado y Cocinero

---
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP4/Punto14/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.Punto14;
+
+import TP4.Punto13.*;
+
+/**
+ *
+ * @author mausa
+ */
+public class Persona {
+    
+    public String nombre;
+
+    public Persona(String nom) {
+        this.nombre = nom;
+    } 
+}
