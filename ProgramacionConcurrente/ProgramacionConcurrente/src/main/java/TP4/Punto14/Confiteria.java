da651637852d782cde149f90ad0d802fa73cf1b6
mausa <mausa@192.168.0.49>
2020-10-13 10:34:35 -0300

    Agregue mutexMozo y mutexCocinero

--- ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP4/Punto14/Confiteria.java
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TP4/Punto14/Confiteria.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
  */
 public class Confiteria {
 
-    Semaphore semMozo, semEmpleado, mutexSillas, semCocinero;
+    Semaphore mutexSillas, mutexMozo, mutexCocinero, semMozo, semCocinero, semComida, semBebida, semComidaYBebida;
     boolean bebida, comida;
     int sillasLibres = 2;
     String miNombre;
@@ -23,13 +23,18 @@ public class Confiteria {
     public Confiteria(String nombreConf, int cantSillas) {
 
         this.mutexSillas = new Semaphore(1, true);
+        this.mutexMozo = new Semaphore(1, true);
+        this.mutexCocinero = new Semaphore(1, true);
         this.semMozo = new Semaphore(0);
-        this.semEmpleado = new Semaphore(0);
+        this.semBebida = new Semaphore(0);
+        this.semComida = new Semaphore(0);
+        this.semComidaYBebida = new Semaphore(0);
         this.semCocinero = new Semaphore(0);
         this.miNombre = nombreConf;
         this.sillasLibres = cantSillas;
 
     }
+
     //El empleado entra a la Confiteria y pide la carta
     //Si no consigue una silla para sentarse se va.
     public boolean entrar(String nombreEmpleado) {
@@ -45,61 +50,59 @@ public class Confiteria {
                 sillasLibres--;
                 pudeEntrar = true;
                 System.out.println("Hola soy " + nombreEmpleado + " quisiera ver la carta por favor.");
-                viendoLaCarta(nombreEmpleado);
+                System.out.println("Soy " + nombreEmpleado + " estoy mirando la carta.");
             } else {
-                System.out.println("Soy " + nombreEmpleado + " no encontre sillas disponibles para sentarme. ME VOY!");
+                System.out.println("Soy " + nombreEmpleado + " no encontre sillas disponibles para sentarme. Vuelvo luego");
             }
         } catch (InterruptedException ex) {
             Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+    
         mutexSillas.release();
         return (pudeEntrar);
     }
 
-    
-    public void viendoLaCarta(String nombreEmpleado) {
-        try {
-            System.out.println("Soy " + nombreEmpleado + " estoy mirando la carta");
-            Thread.sleep(1500);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
+//    public void viendoLaCarta(String nombreEmpleado) {
+//        try {
+//            System.out.println("Soy " + nombreEmpleado + " estoy mirando la carta");
+//            Thread.sleep(1500);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     //El empleado le avisa al mozo que quiere una bebida.
     public void pedirBebida(String nombreEmpleado) {
         try {
             System.out.println("Soy " + nombreEmpleado + " quisiera pedir una bebida. Voy a llamar al mozo.");
+            mutexMozo.acquire();
             semMozo.release();
-            semEmpleado.acquire();
+            semBebida.acquire();
         } catch (InterruptedException ex) {
             Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
         }
+        mutexMozo.release();
     }
-    
+
     //El empleado le avisa al mozo que quiere pedir comida.
     public void pedirComida(String nombreEmpleado) {
         try {
             System.out.println("Soy " + nombreEmpleado + " quisiera pedir comida. Voy a llamar al cocinero.");
+            mutexCocinero.acquire();
             semCocinero.release();
-            semEmpleado.acquire();
+            semComida.acquire();
         } catch (InterruptedException ex) {
             Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
         }
+        mutexCocinero.release();
     }
+
     //Esta opcion se ejecuta cuando el Empleado desea beber y comer.
-        public void pedirComidaYBebida(String nombreEmpleado) {
-        try {
-            System.out.println("Soy " + nombreEmpleado + " quisiera pedir bebida y comida. Voy a llamar al mozo y luego al cocinero.");
-            semMozo.release();
-            semCocinero.release();
-            semEmpleado.acquire();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void pedirComidaYBebida(String nombreEmpleado) {
+        System.out.println("Soy " + nombreEmpleado + " quisiera pedir bebida y comida. Voy a llamar al mozo y luego al cocinero.");
+        this.pedirBebida(nombreEmpleado);
+        this.pedirComida(nombreEmpleado);
     }
-    
+       
     //Tiempo libre del mozo.
     public void mozoEsperaEmpleado(String nombreMozo) {
         System.out.println("Soy el mozo " + nombreMozo + " me voy a inventar nuevas versiones de pollo.");
@@ -109,7 +112,7 @@ public class Confiteria {
             Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     //Tiempo libre del cocinero.
     public void cocineroEsperaEmpleado(String nombreCocinero) {
         System.out.println("Soy el cocinero " + nombreCocinero + " me voy a limpiar la cocina y a inventar nuevas recetas!");
@@ -121,17 +124,23 @@ public class Confiteria {
     }
 
     public void terminarAtencionMozo() {
-        semEmpleado.release();
+        semBebida.release();
     }
 
     public void terminarAtencionCocinero() {
-        semEmpleado.release();
+        semComida.release();
     }
-    
+
     //Cuando el empleado se va, desocupa una silla dejando que otro hilo la ocupe.
     public void salir(String nombre) {
-        semEmpleado.release();
-        sillasLibres++;
+        try {
+            mutexSillas.acquire();
+            sillasLibres++;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Confiteria.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        mutexSillas.release();
     }
 
 }
