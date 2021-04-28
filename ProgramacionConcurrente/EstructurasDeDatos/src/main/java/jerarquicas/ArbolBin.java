ab3e458d066e4b2f1602dab69bdd0c20b4136aaf
mausa <mausa@192.168.0.49>
2021-04-28 20:49:11 -0300

    Se agregaron varios metodos

--- ProgramacionConcurrente/EstructurasDeDatos/src/main/java/jerarquicas/ArbolBin.java
+++ ProgramacionConcurrente/EstructurasDeDatos/src/main/java/jerarquicas/ArbolBin.java
@@ -16,7 +16,7 @@ public class ArbolBin {
     public ArbolBin() {
         this.raiz = null;
     }
-            
+
     public boolean insertar(Object elemNuevo, Object elemPadre, char lugar) {
         boolean exito = true;
         // inserta elemNuevo como hijo del primero nodo encontrado (en preorden) igual a elemPadre
@@ -79,7 +79,7 @@ public class ArbolBin {
     }
 
     // solo busca el padre del nodo raiz
-   public Object padre(Object elemento) {
+    public Object padre(Object elemento) {
         return (this.raiz == null || this.raiz.getElem().equals(elemento)) ? null : padreAux(this.raiz, elemento);
     }
 
@@ -92,8 +92,9 @@ public class ArbolBin {
                 res = nodo.getElem();
             } else {
                 res = padreAux(nodo.getIzquierdo(), elemento);
-                if (res == null)
+                if (res == null) {
                     res = padreAux(nodo.getDerecho(), elemento);
+                }
             }
         }
         return res;
@@ -173,56 +174,67 @@ public class ArbolBin {
             NodoArbol izq = nodo.getIzquierdo();
             NodoArbol der = nodo.getDerecho();
             s += "HI: " + ((izq != null) ? izq.getElem() : "-") + "\t"
-               + "HD: " + ((der != null) ? der.getElem() : "-");
+                    + "HD: " + ((der != null) ? der.getElem() : "-");
             s = toStringAux(nodo.getIzquierdo(), s);
             s = toStringAux(nodo.getDerecho(), s);
         }
         return s;
     }
 
+    /**
+     * @PREORDEN
+     */
     public Lista listarPreOrden() {
         Lista lista = new Lista();
-        preOrdenAux(this.raiz, lista);
+        preOrdenAux(this.raiz, 1, lista);
         return lista;
     }
 
-    private void preOrdenAux(NodoArbol nodo, Lista lista) {
+    private void preOrdenAux(NodoArbol nodo, int pos, Lista lista) {
         if (nodo != null) {
-            preOrdenAux(nodo.getDerecho(), lista);
-            preOrdenAux(nodo.getIzquierdo(), lista);
-            lista.insertar(nodo.getElem(), 1);
+            lista.insertar(nodo.getElem(), pos);
+            preOrdenAux(nodo.getIzquierdo(), pos + 1, lista);
+            preOrdenAux(nodo.getDerecho(), lista.longitud() + 1, lista);
         }
     }
 
-    public Lista listarInOrden() {
-        Lista lista = new Lista();
-        inOrdenAux(this.raiz, lista);
-        return lista;
-    }
-
-    private void inOrdenAux(NodoArbol nodo, Lista lista) {
-        if (nodo != null) {
-            inOrdenAux(nodo.getDerecho(), lista);
-            lista.insertar(nodo.getElem(), 1);
-            inOrdenAux(nodo.getIzquierdo(), lista);
-        }
-    }
-
+    /**
+     * @POSORDEN
+     */
     public Lista listarPosOrden() {
-        Lista lista = new Lista();
-        posOrdenAux(this.raiz, lista);
-        return lista;
+        Lista lista2 = new Lista();
+        posOrdenAux(this.raiz, lista2);
+        return lista2;
     }
 
-    private void posOrdenAux(NodoArbol nodo, Lista lista) {
+    private void posOrdenAux(NodoArbol nodo, Lista lista2) {
         if (nodo != null) {
-            lista.insertar(nodo.getElem(), 1);			
-            posOrdenAux(nodo.getDerecho(), lista);
-            posOrdenAux(nodo.getIzquierdo(), lista);
+            posOrdenAux(nodo.getIzquierdo(), lista2);
+            posOrdenAux(nodo.getDerecho(), lista2);
+            lista2.insertar(nodo.getElem(), lista2.longitud() + 1);
+        }
+
+    }
+
+    /**
+     * @INORDEN
+     */
+    public Lista listarInOrden() {
+        Lista lista3 = new Lista();
+        inOrdenAux(this.raiz, lista3);
+        return lista3;
+
+    }
+
+    private void inOrdenAux(NodoArbol nodo, Lista lista3) {
+        if (nodo != null) {
+            inOrdenAux(nodo.getIzquierdo(), lista3);
+            lista3.insertar(nodo.getElem(), lista3.longitud() + 1);
+            inOrdenAux(nodo.getDerecho(), lista3);
         }
     }
-    
-        public Lista obtenerAncestros(Object elemento) {
+
+    public Lista obtenerAncestros(Object elemento) {
         Lista lista = new Lista();
         obtenerAncestrosAux(this.raiz, lista, elemento);
         // dependiendo de la salida que se desea se invierte o no
@@ -236,8 +248,8 @@ public class ArbolBin {
             if (nodo.getElem().equals(elemento)) {
                 encontrado = true;
                 //Se encuentra a la izquierda o a la derecha del nodo actual?
-            } else if (obtenerAncestrosAux(nodo.getIzquierdo(), lista, elemento) 
-                      || obtenerAncestrosAux(nodo.getDerecho(), lista, elemento)) {
+            } else if (obtenerAncestrosAux(nodo.getIzquierdo(), lista, elemento)
+                    || obtenerAncestrosAux(nodo.getDerecho(), lista, elemento)) {
                 // verifica si se encuentra a la izquierda, aprovecho el corto circuito para en caso de que este, no se busque
                 // caso contrario se ejecutara el siguiente llamado recursivo
                 lista.insertar(nodo.getElem(), 1);
@@ -246,7 +258,6 @@ public class ArbolBin {
         }
         return encontrado;
     }
-
 
     public Lista niveles() {
         Cola cola = new Cola();
@@ -258,10 +269,12 @@ public class ArbolBin {
             nodo = (NodoArbol) cola.obtenerFrente();
             cola.sacar();
             pila.apilar(nodo.getElem());
-            if (nodo.getIzquierdo() != null)
+            if (nodo.getIzquierdo() != null) {
                 cola.poner(nodo.getIzquierdo());
-            if (nodo.getDerecho() != null)
+            }
+            if (nodo.getDerecho() != null) {
                 cola.poner(nodo.getDerecho());
+            }
         }
         while (pila.obtenerTope() != null) {
             lista.insertar(pila.obtenerTope(), 1);
@@ -269,7 +282,36 @@ public class ArbolBin {
         }
         return lista;
     }
-    
+
+    /*
+    Verifica que el árbol otro sea igual al arbol this. 
+    No puede usar los métodos básicos del TDA (insertar, existe, etc) 
+    y debe de recorrer lo mínimo ambos árboles.
+     */
+    public boolean equals(ArbolBin otro) {
+        return equalsAux(this.raiz, otro.raiz);
+    }
+
+    private boolean equalsAux(NodoArbol nodoA1, NodoArbol nodoA2) {
+        boolean res = true;
+        if (nodoA1 != null && nodoA2 != null) {
+            if (!nodoA1.getElem().equals(nodoA2.getElem())) {
+                res = false;
+            } else {
+                res = equalsAux(nodoA1.getIzquierdo(), nodoA2.getIzquierdo());
+
+                if (res) {
+                    res = equalsAux(nodoA1.getDerecho(), nodoA2.getDerecho());
+                }
+            }
+        }
+
+        return res;
+    }
+
+    //Metodo que me devuelve una lista con todas las hojas del arbol
+    //el orden es n porque se visita una vez cada nodo del arbol 
+    //este seria el texto para justificar el orden
     public Lista frontera() {
         Lista lista = new Lista();
 
@@ -288,8 +330,69 @@ public class ArbolBin {
             } else {
                 fronteraRec(a.getDerecho(), lista2, pos);
                 fronteraRec(a.getIzquierdo(), lista2, pos);
-
             }
+        }
+    }
+
+    /**
+     * Metodo que recibe por parámetro una lista patron y determina si coincide
+     * exactamente con al menos un camino del árbol que comience en la raíz y
+     * termine en una hoja
+     */
+    public boolean verificarPatron(Lista listaPatron) {
+        boolean res;
+        res = verificarRecursivo(this.raiz, listaPatron, 1);
+        return res;
+
+    }
+
+    private boolean verificarRecursivo(NodoArbol nodo, Lista listaPatron, int posicion) {
+        boolean aux = false;
+
+        if (posicion > listaPatron.longitud()) {
+            aux = true;
+
+        } else if ((nodo != null) && (nodo.getElem()).equals(listaPatron.recuperar(posicion))) {
+
+            aux = verificarRecursivo(nodo.getIzquierdo(), listaPatron, posicion + 1);
+
+            if (!aux) {
+                aux = verificarRecursivo(nodo.getDerecho(), listaPatron, posicion + 1);
+            }
+
+        }
+        return aux;
+    }
+
+    /*
+    clonarInvertido() que devuelve un nuevo árbol, que es una copia del árbol original(this) pero donde los hijos   
+    están cambiados de lugar. Atención: el método devuelve un nuevo árbol, sin modificar el árbol original.
+     */
+    public ArbolBin clonarInvertido() {
+        ArbolBin arbolClon = new ArbolBin();
+        if (this.raiz != null) {
+            arbolClon.raiz = new NodoArbol(this.raiz.getElem(), null, null);
+            clonarInvertidoAux(this.raiz, arbolClon.raiz);
+        }
+        return arbolClon;
+    }
+
+    private void clonarInvertidoAux(NodoArbol nOrig, NodoArbol nClon) {
+        if (nOrig.getDerecho() != null) {
+            //Si el HD del arbol original tiene elemento
+            //Creo un nuevo nodo y a ese nodo le seteo el elemento HD del arbol original
+            //Ese nuevo nodo creado se lo pongo como HI al arbolClon
+            nClon.setIzquierdo(new NodoArbol(nOrig.getDerecho().getElem(), null, null));
+            //Llamo recursivamente para hacer lo mismo con toda la rama izquierda del arbol original
+            clonarInvertidoAux(nOrig.getDerecho(), nClon.getIzquierdo());
+        }
+        if (nOrig.getIzquierdo() != null) {
+            //Si el HI del arbol original tiene elemento
+            //Creo un nuevo nodo y a ese nodo le seteo el elemento HI del arbol original
+            //Ese nuevo nodo creado se lo pongo como HD al arbolClon
+            nClon.setDerecho(new NodoArbol(nOrig.getIzquierdo().getElem(), null, null));
+            //Llamo recursivamente para hacer lo mismo con toda la rama derecha del arbol original
+            clonarInvertidoAux(nOrig.getIzquierdo(), nClon.getDerecho());
         }
     }
 }
