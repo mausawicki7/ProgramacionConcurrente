6b087b58d1032dcce3dae4e68098cb253ce6c6d0
mausa <mausa@192.168.0.49>
2021-09-30 18:43:09 -0300

    No se bajan los pasajeros del tren

---
+++ ProgramacionConcurrente/ProgramacionConcurrente/src/main/java/TrenTuristico/Vendedor.java
@@ -0,0 +1,44 @@
+/*
+ * Mauricio Sawicki
+ */
+package TrenTuristico;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mausa
+ */
+public class Vendedor implements Runnable{
+    
+    Tren unTren;
+    
+    public Vendedor(Tren t1){
+        this.unTren = t1;
+    }
+    
+    public void run(){
+        while(true){
+            try {
+                
+                unTren.venderTickets();
+                this.venderTicket();
+                unTren.entregarTicket();
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Vendedor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+        public void venderTicket(){
+        System.out.println("Vendedor: Estoy emitiendo el ticket");
+        try {
+            Thread.sleep((int) (Math.random() * 1000));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Pasajero.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
